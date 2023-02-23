ce7b615478f5a490d525e6d2be78a02dabcd7e06
Felipe Emerson <felipe.calixto@ccc.ufcg.edu.br>
2023-02-23 10:32:52 -0300

    initial config of ali-server

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const app = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+
+app.get('/', async (req, res) => {
+    return res.send('<div style="display: flex; justify-content: center; height: 100vh">' + 
+    '<h1 style="align-self: center; -webkit-text-stroke: 1px #000; color: #70b2ff; font-size: 72px">It\'s Working!!!</h1>'+
+    '</div>');
+});
+
+app.get('/aliexpress-product-price', async (req, res) => {
+    const { url, product_options } = req.body;
+
+    const response = await fetch(url);
+    const html = await response.text();
+
+    const dataStr = html.match(/data: ({.+})/)?.[1];
+
+    const data = JSON.parse(dataStr);
+    
+    const index = data.skuModule.skuPriceList.findIndex(sku => product_options.map(t => sku.skuAttr.includes(t)).every(Boolean));
+
+    const skuVal = data.skuModule.skuPriceList[index].skuVal;
+
+    const price = skuVal.isActivity ? skuVal.skuActivityAmount.value : skuVal.skuAmount.value;
+
+    return res.send({ price });
+});
+
+app.get('*', async (req, res) => {
+    return res.status(404).send('<div style="display: flex; flex-direction: column; justify-content: center; height: 100vh">' + 
+    '<h1 style="align-self: center; -webkit-text-stroke: 1px #000; font-size: 64px; color: #d75413">Error 404!</h1>'+
+    '<h2 style="align-self: center; -webkit-text-stroke: 1px #000; font-size: 48px; color: #d75413">Not Found.</h3>'+
+    '</div>');
+});
+
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, () => {
+    console.log(`Now listening on port ${port}`); 
+});
