68cc790c90d59716c53b179d2b6abb5d52d860fe
Felipe Emerson <felipe.calixto@ccc.ufcg.edu.br>
2023-02-23 17:20:23 -0300

    add logs

--- index.js
+++ index.js
@@ -27,9 +27,11 @@ app.post('/aliexpress-product-price', async (req, res) => {
         const dataStr = html.match(/data: ({.+})/)?.[1];
         const data = JSON.parse(dataStr);
 
-        console.log(`Data: ${data}`);
+        console.log(`Keys of data: ${Object.keys(data)}`);
         
         const index = data.skuModule.skuPriceList.findIndex(sku => product_options.map(t => sku.skuAttr.includes(t)).every(Boolean));
+
+        console.log(`Keys of the item: ${Object.keys(data.skuModule.skuPriceList[index])}`);
     
         const skuVal = data.skuModule.skuPriceList[index].skuVal;
     
